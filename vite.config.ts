56a655ca2e5f2c47bdb131bb932a21ebe5c788c6
llucas.spot <llucas.spot@gmail.com>
2024-11-17 23:22:12 +0100

    init check:all

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,25 @@
 import react from '@vitejs/plugin-react';
-import path from "path";
-import {defineConfig} from 'vite';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 function build(relativePath: string) {
-    return path.resolve(__dirname, relativePath)
+  return path.resolve(__dirname, relativePath);
 }
 
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '#lib': build('src/lib'),
-            "#assets": build("src/assets"),
-            "#components": build("src/components"),
-            "#features/auth": build("src/features/auth"),
-            "#features/dashboard": build("src/features/dashboard"),
-            "#features/home": build("src/features/home"),
-            "#features/projects": build("src/features/projects"),
-            "#layout": build("src/layout"),
-            "#lib/auth": build("src/lib/auth"),
-            "#routes": build("src/routes")
-        },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '#lib': build('src/lib'),
+      '#assets': build('src/assets'),
+      '#components': build('src/components'),
+      '#features/auth': build('src/features/auth'),
+      '#features/dashboard': build('src/features/dashboard'),
+      '#features/home': build('src/features/home'),
+      '#features/projects': build('src/features/projects'),
+      '#layout': build('src/layout'),
+      '#lib/auth': build('src/lib/auth'),
+      '#routes': build('src/routes'),
     },
+  },
 });
