22747088b22effcccdabf47e841cb01508dad56f
llucas.spot <llucas.spot@gmail.com>
2024-12-31 06:08:46 +0100

    chore: add VITE_BASE_PATH for path proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 import path from 'path';
 import { defineConfig } from 'vite';
 
@@ -6,9 +7,17 @@ function build(relativePath: string) {
   return path.resolve(__dirname, relativePath);
 }
 
+dotenv.config();
+
+// TODO
+const basepath = process.env.VITE_BASE_PATH ?? '/';
+console.log('basepath : ', basepath);
+
 export default defineConfig({
   plugins: [react()],
+  base: basepath,
   server: {
+    host: '0.0.0.0',
     port: 3001,
   },
   resolve: {
